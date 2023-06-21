11d659a4188b36f0d15259e7996911928c907517
Cole Easton <coea@odoo.com>
2023-06-21 10:52:49 -0400

    Make initial attempt at group exercise 7

---
+++ GE7/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Group Exercise 07",
+    
+    "summary": "Automatically name baed on make, model, and year",
+    
+    "description": """
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry"],
+    "data": [],
+    
+    "demo": [],
+    
+    "author": "Cole Easton, Abhay Arnikar",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
