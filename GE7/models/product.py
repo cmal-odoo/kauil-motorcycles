1a9849183561086fea8c2f2fc90d8975a2c5dd93
Cole Easton <coea@odoo.com>
2023-06-22 09:52:52 -0400

    Make GE07 work correctly

---
+++ GE7/models/product.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models;
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    name = fields.Char(compute="_compute_name", store=True)
+
+    @api.depends("year", "make", "model")
+    def _compute_name(self):
+        for record in self:
+            if record.detailed_type=="motorcycle":
+                record.name = str(record.year)+" "+str(record.make)+" "+str(record.model)
+
