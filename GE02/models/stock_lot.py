fb310cc8f22874df69c9963cc60034d7d282def4
mihd-odoo <mihd@odoo.com>
2023-06-26 14:05:29 -0400

    update GE04

--- GE02/models/stock_lot.py
+++ GE02/models/stock_lot.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models
 class StockLot(models.Model):
     _inherit = 'stock.lot'
 
-    registry_id = fields.Many2one(comodel_name='motorcycle.registry')
+    registry_id = fields.Many2one(comodel_name='motorcycle.registry', store=True)
