728dd4587ac80212787a4196bc425ea360d659e9
abar-odoo <abar@odoo.com>
2023-06-28 11:11:37 -0400

    ge 11

---
+++ GE11/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Group Exercise 11",
+    "summary": "Automatic warehouse selection",
+    "description": """
+    Set the warehouse closest to the user's address as the delivery warehouse
+    """,
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry", "sale_stock"],
+    "data": [],
+    "demo": [],
+    "author": "Abhay Arnikar",
+    "website": "www.odoo.com",
+    "application": True,
+}
